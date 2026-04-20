a967cd8fff0f5a0d44020c310926cd54f91c40d5
Sanyam Kothari <samwork182004@gmail.com>
2026-04-21 01:12:08 +0530

    Add synchronous ASGI exception trapper

--- api/index.py
+++ api/index.py
@@ -1,5 +1,21 @@
+import traceback
 import sys
 import os
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from backend.main import app
+async def app(scope, receive, send):
+    try:
+        from backend.main import app as real_app
+        await real_app(scope, receive, send)
+    except Exception as e:
+        err = traceback.format_exc().encode('utf-8')
+        await send({
+            'type': 'http.response.start',
+            'status': 500,
+            'headers': [[b'content-type', b'text/plain']],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': err,
+        })
