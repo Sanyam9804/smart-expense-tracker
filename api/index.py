9d4c063c45faf3370941b66413e692d34ae68d17
Sanyam Kothari <samwork182004@gmail.com>
2026-04-21 01:00:05 +0530

    Add traceback debugging to Vercel API entrypoint

--- api/index.py
+++ api/index.py
@@ -1,1 +1,18 @@
-from backend.main import app
+import traceback
+
+try:
+    from backend.main import app
+except Exception as e:
+    err_str = traceback.format_exc()
+    
+    async def app(scope, receive, send):
+        assert scope['type'] == 'http'
+        await send({
+            'type': 'http.response.start',
+            'status': 500,
+            'headers': [[b'content-type', b'text/plain']],
+        })
+        await send({
+            'type': 'http.response.body',
+            'body': err_str.encode('utf-8'),
+        })
