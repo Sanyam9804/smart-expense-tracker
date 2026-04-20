123521558a2059db2ea5039aa049f806ee3563a1
Sanyam Kothari <samwork182004@gmail.com>
2026-04-21 01:13:18 +0530

    Downgrade bcrypt to 3.2.0 to bypass Rust compiler lambda crash

--- api/index.py
+++ api/index.py
@@ -1,21 +1,6 @@
-import traceback
 import sys
 import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-async def app(scope, receive, send):
-    try:
-        from backend.main import app as real_app
-        await real_app(scope, receive, send)
-    except Exception as e:
-        err = traceback.format_exc().encode('utf-8')
-        await send({
-            'type': 'http.response.start',
-            'status': 500,
-            'headers': [[b'content-type', b'text/plain']],
-        })
-        await send({
-            'type': 'http.response.body',
-            'body': err,
-        })
+from backend.main import app
